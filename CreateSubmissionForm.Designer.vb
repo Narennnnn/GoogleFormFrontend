915224862ab149ff2522b96cc0e7bf47ce5a2cb9
Narendra Maurya <mauryanarendra2003@gmail.com>
2024-06-20 15:22:59 +0530

    feat: create form working, needs improvement

--- CreateSubmissionForm.Designer.vb
+++ CreateSubmissionForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class CreateSubmissionForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,144 @@ Partial Class CreateSubmissionForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.txtGithubLink = New System.Windows.Forms.TextBox()
+        Me.btnStartStopwatch = New System.Windows.Forms.Button()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.lblStopwatch = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.lblPhone = New System.Windows.Forms.Label()
+        Me.lblGithubLink = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(50, 50)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(200, 20)
+        Me.txtName.TabIndex = 0
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(50, 100)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(200, 20)
+        Me.txtEmail.TabIndex = 1
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(50, 150)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(200, 20)
+        Me.txtPhone.TabIndex = 2
+        '
+        'txtGithubLink
+        '
+        Me.txtGithubLink.Location = New System.Drawing.Point(50, 200)
+        Me.txtGithubLink.Name = "txtGithubLink"
+        Me.txtGithubLink.Size = New System.Drawing.Size(200, 20)
+        Me.txtGithubLink.TabIndex = 3
+        '
+        'btnStartStopwatch
+        '
+        Me.btnStartStopwatch.Location = New System.Drawing.Point(50, 250)
+        Me.btnStartStopwatch.Name = "btnStartStopwatch"
+        Me.btnStartStopwatch.Size = New System.Drawing.Size(200, 30)
+        Me.btnStartStopwatch.TabIndex = 4
+        Me.btnStartStopwatch.Text = "Start/Stop Stopwatch"
+        Me.btnStartStopwatch.UseVisualStyleBackColor = True
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(50, 330)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(200, 30)
+        Me.btnSubmit.TabIndex = 5
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'lblStopwatch
+        '
+        Me.lblStopwatch.AutoSize = True
+        Me.lblStopwatch.Location = New System.Drawing.Point(50, 290)
+        Me.lblStopwatch.Name = "lblStopwatch"
+        Me.lblStopwatch.Size = New System.Drawing.Size(49, 13)
+        Me.lblStopwatch.TabIndex = 6
+        Me.lblStopwatch.Text = "00:00:00"
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(50, 30)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(35, 13)
+        Me.lblName.TabIndex = 7
+        Me.lblName.Text = "Name"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(50, 80)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(32, 13)
+        Me.lblEmail.TabIndex = 8
+        Me.lblEmail.Text = "Email"
+        '
+        'lblPhone
+        '
+        Me.lblPhone.AutoSize = True
+        Me.lblPhone.Location = New System.Drawing.Point(50, 130)
+        Me.lblPhone.Name = "lblPhone"
+        Me.lblPhone.Size = New System.Drawing.Size(78, 13)
+        Me.lblPhone.TabIndex = 9
+        Me.lblPhone.Text = "Phone Number"
+        '
+        'lblGithubLink
+        '
+        Me.lblGithubLink.AutoSize = True
+        Me.lblGithubLink.Location = New System.Drawing.Point(50, 180)
+        Me.lblGithubLink.Name = "lblGithubLink"
+        Me.lblGithubLink.Size = New System.Drawing.Size(64, 13)
+        Me.lblGithubLink.TabIndex = 10
+        Me.lblGithubLink.Text = "GitHub Link"
+        '
+        'CreateSubmissionForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "CreateSubmissionForm"
+        Me.ClientSize = New System.Drawing.Size(284, 401)
+        Me.Controls.Add(Me.lblGithubLink)
+        Me.Controls.Add(Me.lblPhone)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.lblStopwatch)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.btnStartStopwatch)
+        Me.Controls.Add(Me.txtGithubLink)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtName)
+        Me.Name = "CreateSubmissionForm"
+        Me.Text = "Create Submission"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents txtPhone As TextBox
+    Friend WithEvents txtGithubLink As TextBox
+    Friend WithEvents btnStartStopwatch As Button
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents lblStopwatch As Label
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblEmail As Label
+    Friend WithEvents lblPhone As Label
+    Friend WithEvents lblGithubLink As Label
 End Class
--- CreateSubmissionForm.vb
+++ CreateSubmissionForm.vb
@@ -1,3 +1,54 @@
-﻿Public Class CreateSubmissionForm
+﻿Imports System.Net.Http
+Imports System.Text
+Imports Newtonsoft.Json
 
+Public Class CreateSubmissionForm
+    Private stopwatch As New Stopwatch()
+
+    Private Sub CreateSubmissionForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.KeyPreview = True
+    End Sub
+
+    Private Sub btnStartStopwatch_Click(sender As Object, e As EventArgs) Handles btnStartStopwatch.Click
+        If stopwatch.IsRunning Then
+            stopwatch.Stop()
+        Else
+            stopwatch.Start()
+        End If
+        UpdateStopwatchLabel()
+    End Sub
+
+    Private Sub UpdateStopwatchLabel()
+        lblStopwatch.Text = stopwatch.Elapsed.ToString("hh\:mm\:ss")
+    End Sub
+
+    Private Async Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim submission As New With {
+            .name = txtName.Text,
+            .email = txtEmail.Text,
+            .phone = txtPhone.Text,
+            .github_link = txtGithubLink.Text,
+            .stopwatch_time = stopwatch.Elapsed.ToString("hh\:mm\:ss")
+        }
+
+        Dim json As String = JsonConvert.SerializeObject(submission)
+        Dim content As New StringContent(json, Encoding.UTF8, "application/json")
+
+        Using client As New HttpClient()
+            Dim response As HttpResponseMessage = Await client.PostAsync("http://localhost:3000/submit", content)
+            If response.IsSuccessStatusCode Then
+                MessageBox.Show("Submission successful!")
+            Else
+                MessageBox.Show("Failed to submit: " & response.ReasonPhrase)
+            End If
+        End Using
+    End Sub
+
+    Private Sub CreateSubmissionForm_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.Control AndAlso e.KeyCode = Keys.S Then
+            btnSubmit.PerformClick()
+        ElseIf e.Control AndAlso e.KeyCode = Keys.T Then
+            btnStartStopwatch.PerformClick()
+        End If
+    End Sub
 End Class
